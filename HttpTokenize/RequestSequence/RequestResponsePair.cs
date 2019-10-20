0ab999733bc9d152b3860dc3180649af3875993a
Max Kerr <richard.m.kerr@vanderbilt.edu>
2019-10-20 16:05:52 -0700

    Add missing files

---
+++ HttpTokenize/RequestSequence/RequestResponsePair.cs
@@ -0,0 +1,31 @@
+﻿using HttpTokenize.Tokenizers;
+using HttpTokenize.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpTokenize.RequestSequence
+{
+    public class RequestResponsePair
+    {
+        public Request Request { get; }
+        public Response Response { get; }
+
+        public RequestResponsePair(Request request, Response response)
+        {
+            Request = request;
+            Response = response;
+            InputTokens = null;
+            OutputTokens = null;
+        }
+
+        public void Tokenize(List<IRequestTokenizer> requestTokenizers, List<IResponseTokenizer> responseTokenizers)
+        {
+            InputTokens = Request?.GetRequirements(requestTokenizers);
+            OutputTokens = Response?.Tokenize(responseTokenizers);
+        }
+
+        public TokenCollection? InputTokens { get; set; }
+        public TokenCollection? OutputTokens { get; set; }
+    }
+}
---
+++ HttpTokenize/RequestSequence/Stage.cs
@@ -0,0 +1,28 @@
+﻿using HttpTokenize.Substitutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpTokenize.RequestSequence
+{
+    // Holds a request and a sequence of substitutions into that request.
+    // Execution sequence will be 'perform substitutions' -> 'execute request'
+    public class Stage
+    {
+        public List<ISubstitution> Substitutions { get; }
+        public Request Request { get; }
+
+        public Stage(Request request)
+        {
+            Request = request;
+            Substitutions = new List<ISubstitution>();
+        }
+
+        public Stage Copy()
+        {
+            Stage stage = new Stage(Request);
+            stage.Substitutions.AddRange(Substitutions);
+            return stage;
+        }
+    }
+}
