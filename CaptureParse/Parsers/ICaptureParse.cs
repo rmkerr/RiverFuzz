4701e536c753d7e2b8a6bca8ad1fc159bc39aeae
Max Kerr <richard.m.kerr@vanderbilt.edu>
2020-02-24 23:51:44 -0800

    Make interface public.

--- CaptureParse/Parsers/ICaptureParse.cs
+++ CaptureParse/Parsers/ICaptureParse.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace CaptureParse.Parsers
 {
-    interface ICaptureParse
+    public interface ICaptureParse
     {
         KnownEndpoint ParseSingleRequestFile(string content, string host);
     }
