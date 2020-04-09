31a561bbff1aff8426f9b52c266e506ff88092a4
Matthew Takemoto <matt.t.takemoto@gmail.com>
2020-04-08 17:49:28 -0700

    Now everything is a container

--- Database/DatabaseHelper.cs
+++ Database/DatabaseHelper.cs
@@ -17,23 +17,24 @@ namespace Database
     public class DatabaseHelper : DatabaseCore
     {
         private string DbName;
-        private bool Production;
+        private bool IsProduction;
 
         public DatabaseHelper(string dbname, bool production)
         {
             DbName = dbname;
-            Production = production;
+            IsProduction = production;
         }
 
+        //TODO: we should look at putting these in a config class or something
         internal IDbConnection GetConnection(string DbName)
         {
-            if (Production)
+            if (IsProduction)
             {
                 return new NpgsqlConnection($"Server=riverfuzz-postgres.postgres.database.azure.com;Database={DbName};Port=5432;User Id=postgres@riverfuzz-postgres;Password='3r!T8*Qb8YNFlG8Eb8u';Ssl Mode=Require;");
             }
             else
             {
-                return new NpgsqlConnection($"Server=localhost;Database={DbName};Port=5432;User Id=postgres;Password='3r!T8*Qb8YNFlG8Eb8u';");
+                return new NpgsqlConnection($"Server=db;Database={DbName};Port=5432;User Id=postgres;Password='3r!T8*Qb8YNFlG8Eb8u';");
             }
         }
 
